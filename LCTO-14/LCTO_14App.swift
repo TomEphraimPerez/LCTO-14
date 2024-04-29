d226b245673a6d1cdb73ea3cb42525e29d9d027a
Thomas Ephraim Perez <greatcfi@aol.com>
2024-04-28 20:19:34 -0700

    Comments and URLs in @mail()

--- LCTO-14/LCTO_14App.swift
+++ LCTO-14/LCTO_14App.swift
@@ -2,8 +2,16 @@
 //  LCTO_14App.swift
 //  LCTO-14
 //
-//  Created by thomasperez on 4/17/24.
-//
+//  Created by thomasperez on 4/17/24. | Swift 5. in toplevel LCTO-14 /LCTO-14.xcodeproj/Build_settings/Swift_compiler_Lang/..ver
+//  For future customer updates/downloads fr AppStore, check their OS version vs my deployment target:
+/*
+ https://developer.apple.com/forums/thread/750845
+  . . . in toplevel LCTO-14 /LCTO-14.xcodeproj/Build_settings/Deployment/Targeted_Device_Families/iOS_deployment_target/
+                (me = iOS 16.2)
+ 
+ SAMPLE progroms >>>
+ https://github.com/apple/sample-cloudkit-sharing
+ */
 
 import SwiftUI
 
