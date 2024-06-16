089bbc6e024342f03fa3f143ec57e4b3eff8803a
Thomas Ephraim Perez <greatcfi@aol.com>
2024-06-16 15:52:52 -0700

    LKGgit status! Shelves OK. TODO Land 1st comment ontop of scroll window

--- LCTO-14/ContentView.swift
+++ LCTO-14/ContentView.swift
@@ -1,5 +1,5 @@
 /*
- Sun 6-16-24)1100
+ Sun 6-16-24)1453
  Test for bug in github ""LKGgit add . All txtfld & button positions OK! TODO Scroll ht too tall""
   Could be excalmation points?? When committing _
    git commit -m "LKG!! All txtfld & button positions OK! TODO Scroll ht too tall"
@@ -17,123 +17,139 @@
  Other Ap = pass to sim
  Record_Type = ProductComment
 */
- 
- 
+
 import SwiftUI
 
+struct ContentView: View {
+    @EnvironmentObject var viewModel: ViewModel
+    
+    var body: some View {
+        VStack {
+            StarView(rating: viewModel.averageRating)
+                .padding(.top, -30)
 
- struct ContentView: View {
-     @EnvironmentObject var viewModel: ViewModel
-     
-     var body: some View {
-         VStack {
-             StarView(rating: viewModel.averageRating)
-                 .padding(.top, -30)
-             TextField("Search for the product here...", text: $viewModel.userInput3)
-                 .frame(width: UIScreen.main.bounds.width * 0.82)                        // Set width to 92% of the screen width
-                 .textFieldStyle(RoundedBorderTextFieldStyle())
-                 .autocapitalization(.none)
+            ZStack {
+                                                                                        // Placing the background under the rest of the UI
+                Image("shelves")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .opacity(0.2)                                                       // Set the opacity to 0.2
+                    .edgesIgnoringSafeArea(.all)                                        // Makes image fill the entire available space
 
-             Button("Search Product") {
-                 viewModel.fetchProductNames(searchTerm: viewModel.userInput3)
-                 viewModel.calculateAverageRating(for: viewModel.userInput3)
-             }
-             .padding()
-             .foregroundColor(.white)
-             .background(Color.blue)
-             .cornerRadius(10)
+                
+                
+                                                            // S E A R C H
 
-             if !viewModel.searchMessage.isEmpty {
-                 Text(viewModel.searchMessage)
-                     .foregroundColor(.red)
-                     .padding()
-             }
-             
-             if !viewModel.searchResults.isEmpty {
-                 GeometryReader { geometry in
-                     ScrollView {
-                         VStack(alignment: .leading) {
-                             ForEach(viewModel.searchResults, id: \.self) { comment in
-                                 Text(comment)
-                                     //.padding()                                            // O. LKG 6-13-24
-                                     .padding(.vertical, 4)                                  // Reduced vertical padding
-                                     .frame(height: 20)                                      // Explicit height for each comment
-                                     .background(Color.gray.opacity(0.3))                    // Was 0.3
-                                     .cornerRadius(2)                                        // Was 5.
-                             }
-                         }
-                     }
-                     .frame(width: geometry.size.width, height: geometry.size.height * 1.2)  // O=1.6->using 66% of available ht
-                     .border(Color.blue, width: 2)                                           // Keep this fr prev <snippet>
-                 }
-             } else {
-                 Text("No results found")
-                     .font(.headline)
-             }
+                VStack {
+                    TextField("Search for the product here...", text: $viewModel.userInput3)
+                        .frame(width: UIScreen.main.bounds.width * 0.82)                // Set width to 82% of the screen width
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .autocapitalization(.none)
 
-             
-             Spacer()                                    // O. Keep. Maintains spacing bt sections if there are no results.
+                    Button("Search Product") {
+                        viewModel.fetchProductNames(searchTerm: viewModel.userInput3)
+                        viewModel.calculateAverageRating(for: viewModel.userInput3)
+                    }
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
 
-             
-             TextField("Enter the product here...", text: $viewModel.userInput)
-                 //.padding(.top, -10.0)                                                     // Was -10.0 (0.7 cm abv usrIP 4)
-                 .frame(width: UIScreen.main.bounds.width * 0.82)                            // Set width to 92% of the screen width
-                 //.padding([.top], UIScreen.main.bounds.height * 0.16)  // Adds padding top, shifting txt fld dwn 16% of the scn ht
-                 //.padding(.top, UIScreen.main.bounds.height * 0.2) // Add 9% of scn ht as top padding. If >>, button lowers too much
-                 .offset(y: UIScreen.main.bounds.height * 0.03) // Adjust this value to position userInput
-                 .textFieldStyle(RoundedBorderTextFieldStyle())
-                 .autocapitalization(.none)
+                    if !viewModel.searchMessage.isEmpty {
+                        Text(viewModel.searchMessage)
+                            .foregroundColor(.red)
+                            .padding()
+                    }
+                    
+                    if !viewModel.searchResults.isEmpty {
+                        GeometryReader { geometry in
+                            ScrollView {
+                                VStack(alignment: .leading) {
+                                    ForEach(viewModel.searchResults, id: \.self) { comment in
+                                        Text(comment)
+                                            .padding(.vertical, 4)                      // Reduced vertical padding
+                                            .frame(height: 20)                          // Explicit height for each comment
+                                            .background(Color.gray.opacity(0.3))        // Was 0.3
+                                            .cornerRadius(2)                            // Was 5.
+                                    }
+                                }
+                                .frame(width: geometry.size.width, height: geometry.size.height * 2.5) // O=1.6->using 66% of available ht
+                                .border(Color.blue, width: 2)
+                            }
+                        }
+                    } else {
+                        Text("No results found")
+                            .font(.headline)
+                    }
 
-         
-             TextField("Enter star rating (0-5)", text: $viewModel.userInput4)
-                             .frame(width: UIScreen.main.bounds.width * 0.82)                // Set width to 92% of the screen width
-                             .keyboardType(.numberPad)
-                             .textFieldStyle(RoundedBorderTextFieldStyle())
-                             .padding()
-             
-             
-             TextField("Enter your comment here...", text: $viewModel.userInput2)
-                 .frame(width: UIScreen.main.bounds.width * 0.92)                            // Set width to 92% of the screen width
-                 .padding(.top, -30)
-                 .textFieldStyle(RoundedBorderTextFieldStyle())
-                 .padding()
-                 .autocapitalization(.none)
-                 .onChange(of: viewModel.userInput2) { newValue in
-                         if newValue.count > 60 {                                            // Adjust the max character limit as needed
-                             viewModel.userInput2 = String(newValue.prefix(60))
-                         }
-                 }
-             
-             
-             Button("Post Comment") {
-                 viewModel.postComment(productName: viewModel.userInput, comment: viewModel.userInput2, rating: viewModel.userInput4)
-             }
-             .padding()
-             .background(Color.blue)
-             .foregroundColor(.white)
-             .cornerRadius(10)
-         }
-         .padding()
-     }
- }
+                    Spacer()                                             // O. Keep. Maintains spacing bt sections if there are no results.
 
- struct StarView: View {
-     var rating: Double
-     
-     var body: some View {
-         HStack {
-             ForEach(0..<5) { index in
-                 Image(systemName: starType(index: index))
-                     .foregroundColor(index < Int(rating) ? .yellow : .gray)
-             }
-         }
-     }
-     
-     private func starType(index: Int) -> String {
-         if Double(index) < rating {
-             return index + 1 <= Int(rating) ? "star.fill" : "star.leadinghalf.fill"
-         } else {
-             return "star"
-         }
-     }
- }
+                    
+                    
+                                                            // P O S T
+                    
+                    TextField("Enter the product here...", text: $viewModel.userInput)
+                        //.padding(.top, -10.0)                          // Was -10.0 (0.7 cm abv usrIP 4)
+                        .frame(width: UIScreen.main.bounds.width * 0.82) // Set width to 82% of the screen width
+                        //.padding([.top], UIScreen.main.bounds.height * 0.16)  // Adds padding top, shifting txt fld dwn 16% of the scn ht
+                        //.padding(.top, UIScreen.main.bounds.height * 0.2)     // Add x% of scn ht as top padding. If >>, button lowers too much
+                        .offset(y: UIScreen.main.bounds.height * 0.03)   // Adjust this value to position userInput
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .autocapitalization(.none)
+
+                    
+                    TextField("Enter star rating (0-5)", text: $viewModel.userInput4)
+                        .frame(width: UIScreen.main.bounds.width * 0.82)                // Set width to 82% of the screen width
+                        .keyboardType(.numberPad)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+
+                    
+                    TextField("Enter your comment here...", text: $viewModel.userInput2)
+                        .frame(width: UIScreen.main.bounds.width * 0.92)
+                        .padding(.top, -30)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                        .autocapitalization(.none)
+                        .onChange(of: viewModel.userInput2) { newValue in
+                            if newValue.count > 60 {                                    // Adjust the max character limit as needed
+                                viewModel.userInput2 = String(newValue.prefix(60))
+                            }
+                        }
+
+                    Button("Post Comment") {
+                        viewModel.postComment(productName: viewModel.userInput, comment: viewModel.userInput2, rating: viewModel.userInput4)
+                    }
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+
+                                                            // STARS
+struct StarView: View {
+    var rating: Double
+
+    var body: some View {
+        HStack {
+            ForEach(0..<5) { index in
+                Image(systemName: starType(index: index))
+                    .foregroundColor(index < Int(rating) ? .yellow : .gray)
+            }
+        }
+    }
+    
+    private func starType(index: Int) -> String {
+        if Double(index) < rating {
+            return index + 1 <= Int(rating) ? "star.fill" : "star.leadinghalf.fill"
+        } else {
+            return "star"
+        }
+    }
+}
