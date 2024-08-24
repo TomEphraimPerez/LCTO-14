d28cafb4d069807f2d7427fd16aa3ec0bbe48426
Thomas Ephraim Perez <greatcfi@aol.com>
2024-08-24 11:51:59 -0700

    Test1; main v Inages

--- LCTO-14/ViewModel.swift
+++ LCTO-14/ViewModel.swift
@@ -83,7 +83,7 @@ final class ViewModel: ObservableObject {
                     let comments = records.compactMap { record -> String? in
                         if let productName = record["productName"] as? String,
                            let comment = record["comment"] as? String,
-                           productName.lowercased().hasPrefix(searchTerm.lowercased()) {
+                           productName.lowercased().hasPrefix(searchTerm.lowercased()) {            // O. Spic and span v Spic and Span. :/
                             print("Comment found: \(comment)")
                             return comment
                         }
